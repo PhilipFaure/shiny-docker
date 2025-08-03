f11396d4bf7aac69079927a0abf8e9a90955892b
Philip <philip.faure13@gmail.com>
2025-08-03 16:39:49 +0200

    readme + app creation

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ This is an example to build a Shiny app and wrap it in a Docker image.
 The repo can be cloned and the shiny app adapted as necessary.
 
 ### Build docker image:
+```{bash}
 docker build -t shiny-docker-image .
+```
 
 ### Run Shiny app:
 ```{bash}
@@ -29,11 +31,12 @@ docker load < shiny-docker-image.tar
 ### Option 2 - Push to Docker Hub
 ### Tag and push (requires Docker Hub account)
 ```{bash}
-docker tag shiny-docker-image yourusername/shiny-docker-image
-docker push yourusername/shiny-docker-image
+docker login
+docker tag shiny-docker-image tierboskat/shiny-docker-image:0.0.1
+docker push tierboskat/shiny-docker-image:0.0.1
 ```
 
 ### The run with:
 ```{bash}
-docker run -p 3838:3838 yourusername/shiny-docker-image
+docker run -p 3838:3838 tierboskat/shiny-docker-image
 ```
--- app/app.R
+++ app/app.R
@@ -1,21 +1,91 @@
+# library(shiny)
+# 
+# ui <- fluidPage(
+#   titlePanel("Hello Shiny!"),
+#   sidebarLayout(
+#     sidebarPanel(
+#       sliderInput("obs", "Number of observations:", 1, 100, 50)
+#     ),
+#     mainPanel(
+#       plotOutput("distPlot")
+#     )
+#   )
+# )
+# 
+# server <- function(input, output) {
+#   output$distPlot <- renderPlot({
+#     hist(rnorm(input$obs))
+#   })
+# }
+# 
+# shinyApp(ui = ui, server = server)
 library(shiny)
+library(DT)
+library(shinyFiles)
+library(shinyjs)
 
 ui <- fluidPage(
-  titlePanel("Hello Shiny!"),
+  useShinyjs(),
+  titlePanel("Video Viewer"),
   sidebarLayout(
     sidebarPanel(
-      sliderInput("obs", "Number of observations:", 1, 100, 50)
+      shinyDirButton("folder", "Choose Folder", "Select a folder with videos"),
+      verbatimTextOutput("folderPath"),
+      DTOutput("videoTable")
     ),
     mainPanel(
-      plotOutput("distPlot")
+      uiOutput("videoPlayer")
     )
   )
 )
 
-server <- function(input, output) {
-  output$distPlot <- renderPlot({
-    hist(rnorm(input$obs))
+server <- function(input, output, session) {
+  volumes <- c(Home = normalizePath("~"))
+  shinyDirChoose(input, "folder", roots = volumes, session = session)
+  
+  folderPath <- reactiveVal(NULL)
+  videoFiles <- reactiveVal(NULL)
+  
+  observeEvent(input$folder, {
+    path <- parseDirPath(volumes, input$folder)
+    if (length(path) > 0 && dir.exists(path)) {
+      folderPath(path)
+      vids <- list.files(path, pattern = "\\.(mp4|webm|ogg)$", full.names = TRUE, ignore.case = TRUE)
+      if (length(vids) > 0) {
+        videoFiles(data.frame(Filename = basename(vids), FullPath = vids, stringsAsFactors = FALSE))
+      } else {
+        videoFiles(NULL)
+      }
+    }
+  })
+  
+  output$folderPath <- renderText({
+    req(folderPath())
+    paste("Selected folder:", folderPath())
+  })
+  
+  output$videoTable <- renderDT({
+    req(videoFiles())
+    datatable(videoFiles()[, "Filename", drop = FALSE], selection = 'single', rownames = FALSE)
+  })
+  
+  output$videoPlayer <- renderUI({
+    vids <- videoFiles()
+    req(vids)
+    selected <- input$videoTable_rows_selected
+    if (length(selected) == 1) {
+      filePath <- vids$FullPath[selected]
+      tags$video(
+        src = normalizePath(filePath, winslash = "/"),
+        type = "video/mp4",
+        controls = NA,
+        width = "100%",
+        autoplay = NA
+      )
+    } else {
+      h4("Select a video to view")
+    }
   })
 }
 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
